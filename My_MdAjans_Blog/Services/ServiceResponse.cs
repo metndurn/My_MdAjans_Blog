c279c6142fd412f379a2d4a1352bc45fe6976f69
metndurn <mtndrn123@gmail.com>
2025-11-09 21:39:08 +0300

    6. ServiceResponse Generic Sınıfının Yazılması

---
+++ My_MdAjans_Blog/Services/ServiceResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace My_MdAjans_Blog.Services
+{
+	public class ServiceResponse <T>//bu bir generic classdır. T tipi ile çalışır. ve bizim olusturdugumumuz servislerin donus tipini belirler.
+	{
+		public bool IsError { get; set; }
+		public List<string> Errors { get; set; } = new List<string>();
+		public T Data { get; set; }
+		public void AddError(string errorMessage)
+		{ 
+			IsError = true;
+			Errors.Add(errorMessage);
+		}
+
+	}
+}
